315792ce5d1fd4236ae76255882a0b697be81a69
Cameron Lares <cameronlares@gmail.com>
2020-09-18 13:25:38 -0400

    MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
+import { useLocation, useParams, useHistory } from "react-router-dom";
+import { axiosWithAuth } from './util/AxiosWithAuth';
 
 const initialColor = {
   color: "",
@@ -11,20 +13,49 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
+  const location = useLocation();
+    const params = useParams();
+    const { push } = useHistory();
+
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
   };
+
+
+
 
   const saveEdit = e => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      // res.data ==> full array with updated item
+      setColorToEdit(res.data);
+      push('/bubble-page');
+      console.log(res.data)
+    })
+    .catch(err => console.log(err));
+
+
   };
+
 
   const deleteColor = color => {
     // make a delete request to delete this color
+  
+    axiosWithAuth()
+    .delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then((res) => {
+     push("/bubble-page");
+      setEditing(res.data);
+      console.log(res.data)
+    })
+    .catch((error) => console.log(error));
   };
 
   return (
@@ -75,7 +106,7 @@ const ColorList = ({ colors, updateColors }) => {
             />
           </label>
           <div className="button-row">
-            <button type="submit">save</button>
+            <button onClick={saveEdit} type="submit">save</button>
             <button onClick={() => setEditing(false)}>cancel</button>
           </div>
         </form>
